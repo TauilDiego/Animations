da0a00a8d572c97e166e8b8a7fa60b55f1b4050d
Macbook pro de Diego <tauildiego@icloud.com>
2025-01-27 16:41:08 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Diego Tauil on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
